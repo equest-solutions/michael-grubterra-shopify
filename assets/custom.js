169006d6d8a91e01e832418d9dba57479cc5f6a2
dhara-v-eq <83581778+dhara-v-eq@users.noreply.github.com>
2026-03-03 17:28:49 +0530

    PDP in-progress

--- assets/custom.js
+++ assets/custom.js
@@ -66,4 +66,108 @@ $(document).ready(function(){
     arrows: true
   });
 
+
+  // On load fill the check value
+  let subs_com_price =$('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-comp-price');
+  let subs_price =$('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-subs-price');
+  let sav_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-save-price');
+  let original_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-original-price');
+  console.log('compare at price-', subs_com_price);
+  console.log('Subscription price-', subs_price);
+  console.log('Save price-', sav_price);
+  $('.bbfreq_radio_wrapper .subscribe-label .frq_info .save-price').text(sav_price);
+  $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-price').text(subs_price);
+  $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-comp-price').text(subs_com_price);
+  $('.product-form__buttons .product-form__submit .product-price .price').text(subs_price);
+  $('.product-form__buttons .product-form__submit .product-price .compare-price').text(subs_com_price);
+  $('.bbfreq_radio_wrapper .frq_info .ot-price').text(original_price);
+
+
+  // On subscribe button chnage prices
+  $('.flock_options .gt-prod-var-box-sub .flock_item input').on('click', function(){
+      let com_p = $(this).attr('data-comp-price');
+      let sub_p = $(this).attr('data-subs-price');
+      let save_p = $(this).attr('data-save-price');
+      let s_price = $(this).attr('data-v-price');
+      let original_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-original-price');
+      let v_id = $(this).attr('data-v-id');
+       $('#buy_box_form .product-variant-id').val(v_id);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .save-price').text(save_p);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-price').text(sub_p);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-comp-price').text(com_p);
+      $('.product-form__buttons .product-form__submit .product-price .price').text(sub_p);
+      $('.product-form__buttons .product-form__submit .product-price .compare-price').text(com_p);
+      $('.bbfreq_radio_wrapper .frq_info .ot-price').text(s_price);      
+  });
+
+  $('.bb_frequancy_inner .bbfreq_radio_wrapper input').on('click',function(){
+    let check_name  = $(this).val();
+    if(check_name == 'onetime'){
+      $('.flock_options.one_time').show();
+      $('.flock_options.subscribe_save').hide();
+      let ot_c_price = $('.flock_options.one_time .gt-prod-var-box-sub .flock_item input:checked').attr('data-comp-price');
+      let ot_original_price = $('.flock_options.one_time .gt-prod-var-box-sub .flock_item input:checked').attr('data-v-price');
+      let ot_save_price = $('.flock_options.one_time .gt-prod-var-box-sub .flock_item input:checked').attr('data-save-price');
+      let sub_original_price = $('.flock_options.one_time .gt-prod-var-box-sub .flock_item input:checked').attr('data-subs-price');
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .save-price').text(ot_save_price);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-price').text(sub_original_price);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-comp-price').text(ot_c_price);
+      $('.product-form__buttons .product-form__submit .product-price .price').text(ot_original_price);
+      $('.product-form__buttons .product-form__submit .product-price .compare-price').text(ot_c_price);
+      $('.bbfreq_radio_wrapper .frq_info .ot-price').text(ot_original_price);
+    }else{
+      $('.flock_options.one_time').hide();
+      $('.flock_options.subscribe_save').show();
+       let ot_c_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-comp-price');
+      let ot_original_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-v-price');
+      let ot_save_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-save-price');
+      let sub_original_price = $('.flock_options.subscribe_save .gt-prod-var-box-sub .flock_item input:checked').attr('data-subs-price');
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .save-price').text(ot_save_price);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-price').text(sub_original_price);
+      $('.bbfreq_radio_wrapper .subscribe-label .frq_info .ss-comp-price').text(ot_c_price);
+      $('.product-form__buttons .product-form__submit .product-price .price').text(ot_original_price);
+      $('.product-form__buttons .product-form__submit .product-price .compare-price').text(ot_c_price);
+      $('.bbfreq_radio_wrapper .frq_info .ot-price').text(ot_original_price);
+    }
+  });
+
+  $('.product-form__submit').on('click',function(e){
+    e.preventDefault();
+    let $this = $(this);
+    $this.attr('disabled');
+    var itemsArray = [];
+    let var_id = $('.product-variant-id').val();
+
+    var fre_op =  $('.bb_frequancy_inner .bbfreq_radio_wrapper input:checked').val();
+    if(fre_op == 'onetime'){
+        var customItem = [
+          {
+            id: var_id,
+            quantity: 1
+          }
+        ];
+      itemsArray.push(...customItem);
+    }else{
+        var selling_plan_id = $('#frquancy option:selected').val();     
+        var customItem = [{
+          id: var_id,
+          quantity: 1,
+          selling_plan: selling_plan_id
+        }];
+        itemsArray.push(customItem);
+    }
+
+    $.ajax({
+        url: '/cart/add.js',
+        method: "POST",
+        dataType: "JSON",
+        data: { items: itemsArray },
+        success: function (data) {
+          $this.removeAttr('disabled');
+        },
+          error: function (data) {
+        }
+    });
+
+  });
 });
